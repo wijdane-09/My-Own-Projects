d19fa208b22ddcb3b5498e9a930ad7edef5bf222
wijdane-09 <wijdanee76@gmail.com>
2026-05-04 19:36:08 +0100

    add simple quiz

---
+++ simple quiz/README.md
@@ -0,0 +1,25 @@
+# Quiz App 
+
+A simple and interactive Quiz Application built using HTML, CSS, and JavaScript.
+
+## Features
+- Multiple choice questions
+- Score calculation
+- Next question functionality
+- Restart quiz option
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Screenshot
+![Quiz App Screenshot](screenshot.png)
+
+## 🔗 Live Demo
+(Add GitHub Pages link here)
+
+## 💡 What I learned
+- DOM manipulation
+- Event handling
+- Working with arrays and objects in JavaScript
---
+++ simple quiz/script.js
@@ -0,0 +1,104 @@
+const questions = document.getElementById('question');
+const answers = document.querySelectorAll('.answer');
+const result = document.getElementById('result');
+const nextBtn = document.getElementById('nextBtn');
+const restartBtn = document.getElementById('restartBtn');
+const progress = document.getElementById('progress'); 
+
+let currentIndex = 0;
+let score = 0;
+
+const quiz = [
+  {
+    question: 'What is JavaScript?',
+    answers: ['A programming language', 'Creating databases', 'Styling web pages'],
+    correct: 'A programming language'
+  },
+  {
+    question: 'What is true',
+    answers: ['String', 'Boolean', 'Number'],
+    correct: 'Boolean'
+  },
+  {
+    question: 'Which symbol is for comments?',
+    answers: ['//', '##', '__'],
+    correct: '//'
+  }
+];
+
+// hide restart
+restartBtn.style.display = 'none';
+
+// show question
+function ShowQuestion() {
+  const current = quiz[currentIndex];
+
+  // ✅ Progress
+  progress.textContent = `Question ${currentIndex + 1} of ${quiz.length}`;
+
+  questions.textContent = current.question;
+  
+  nextBtn.disabled = true;
+
+  answers.forEach((btn, index) => {
+    btn.textContent = current.answers[index];
+    btn.classList.remove('correct', 'wrong');
+    btn.disabled = false;
+    btn.style.display = 'block';
+  });
+  result.textContent = '';
+}
+
+// click answers
+answers.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const current = quiz[currentIndex];
+
+    if (btn.textContent === current.correct) {
+      btn.classList.add('correct');
+      result.textContent = 'Correct ✅';
+      score++;
+    } else {
+      btn.classList.add('wrong');
+      result.textContent = 'Wrong ❌';
+    }
+
+    answers.forEach(b => {
+      if (b.textContent === current.correct) {
+        b.classList.add('correct');
+      }
+      b.disabled = true;
+    });
+    nextBtn.disabled = false
+  });
+});
+
+// next
+nextBtn.addEventListener('click', () => {
+  currentIndex++;
+
+  if (currentIndex < quiz.length) {
+    ShowQuestion();
+  } else {
+    questions.textContent = 'Quiz finished';
+    result.textContent = `Your score: ${score} / ${quiz.length}`;
+    progress.textContent = ''; // ✅ نخبيو progress فالنهاية
+    nextBtn.disabled = true;
+
+    restartBtn.style.display = 'block';
+  }
+});
+
+// restart
+restartBtn.addEventListener('click', () => {
+  currentIndex = 0;
+  score = 0;
+
+  nextBtn.disabled = false;
+  restartBtn.style.display = 'none';
+
+  ShowQuestion();
+});
+
+// start
+ShowQuestion();
